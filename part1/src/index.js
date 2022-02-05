c2632141dbf58dafcfe337c9f80c719958fac7f0
Marcelo Oroquieta <marcelo@oroquieta.com>
2022-02-05 15:54:56 -0300

    data as objects

--- part1/src/index.js
+++ part1/src/index.js
@@ -11,33 +11,46 @@ const Part = (props) => {
 
 const Content = (props) => {
   return (<>
-    <Part part={props.part1} exercises={props.exercises1}/>
-    <Part part={props.part2} exercises={props.exercises2}/>
-    <Part part={props.part3} exercises={props.exercises3}/>
+    <Part part={props.part1.name} exercises={props.part1.exercises}/>
+    <Part part={props.part2.name} exercises={props.part2.exercises}/>
+    <Part part={props.part3.name} exercises={props.part3.exercises}/>
   </>)
 }
 
 const Total = (props) => {
-  return <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  const numberOfExercises = props.part1.exercises + props.part2.exercises + props.part3.exercises;
+  return <p>Number of exercises {numberOfExercises}</p>
 }
 
 const App = () => {
+  // const course = 'Half Stack application development'
+  // const part1 = 'Fundamentals of React'
+  // const exercises1 = 10
+  // const part2 = 'Using props to pass data'
+  // const exercises2 = 7
+  // const part3 = 'State of a component'
+  // const exercises3 = 14
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
-  const contentProps = {part1, part2, part3, exercises1, exercises2, exercises3}
-  const finalProps = {exercises1, exercises2, exercises3}
+  const props = {part1, part2, part3}
 
   return (
     <div>
       <Header course={course} />
-      <Content {...contentProps} />
-      <Total {...finalProps}/>
+      <Content {...props} />
+      <Total {...props}/>
     </div>
   )
 }

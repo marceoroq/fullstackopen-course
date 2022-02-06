0b2ea10eb0090c45fd0cb566c4a9d5218f86c3f8
Marcelo Oroquieta <marcelo@oroquieta.com>
2022-02-06 10:12:35 -0300

    unicafe ex. 1.6 to 1.11

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const Button = ({handleClick, text}) => {
+  return <button onClick={handleClick}>{text}</button>
+}
+
+const Statistics = ({good, neutral, bad}) => {
+  const sum = good + neutral + bad;
+  if (sum === 0) {
+    return "No feedback given"
+  } else {
+    return (
+      <table>
+        <tbody>
+          <tr><td>Good</td><td>{good}</td></tr>
+          <tr><td>Neutral</td><td>{neutral}</td></tr>
+          <tr><td>Bad</td><td>{bad}</td></tr>
+          <tr><td>All</td><td>{sum}</td></tr>
+          <tr><td>Average</td><td>{(good - bad)/sum}</td></tr>
+          <tr><td>Positive</td><td>{(good * 100)/sum}%</td></tr>
+        </tbody>
+      </table>
+    );
+  }
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h2>Give Feedback</h2>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <h2>Statistics</h2>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
